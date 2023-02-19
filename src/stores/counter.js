2b9a6c68068ed70b97dba2a60dee5b9c3f64c61a
ThaiPt <vuthaind@gmail.com>
2023-02-19 19:53:00 +0700

    update3

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -15,7 +15,7 @@ export const useCounterStore = defineStore('counter', {
       Link_Path_Route:{"Home":"/","Web_Template":{Sell_Accounts:"/Product/SellAccounts",Construction_Industry:"/Product/ConstructionIndustry",Cosmetics_Industry:"/Product/CosmeticsIndustry"},Introduce:"/Introduce",Contact:"/Contact"},
       Link_Image:["https://d1hjkbq40fs2x4.cloudfront.net/2017-08-21/files/landscape-photography_1645.jpg","https://img.meta.com.vn/Data/image/2022/01/13/anh-dep-thien-nhien-3.jpg","https://d1hjkbq40fs2x4.cloudfront.net/2020-01-09/files/canon-landscape-photography-preparation-tips_1968-1s.jpg"],
       Oder_Image:0,count:0,Table_Informations:false,Image_Language:'/assets/vn-2ac15179.svg',Text_Array:['Facebook','Zalo','Gọi điện'],class_1:'bg-gray-100 w-[15px] h-[5px] rounded opacity-50',
-      class_2:'bg-gray-100 w-[15px] h-[5px] rounded opacity-100',Data_List_Product:[{'Category_Industry':''}],Data_List_Product_Home:[{Oder_Image:1}],
+      class_2:'bg-gray-100 w-[15px] h-[5px] rounded opacity-100',Data_List_Product:[{'Category_Industry':''}],Data_List_Product_Home:[{Oder_Image:100}],
     }
   },
 
@@ -45,7 +45,7 @@ export const useCounterStore = defineStore('counter', {
         this.Data_List_Product_Home = await this.Data_List_Product_Home.data;
       }
       catch(error){
-        this.Data_List_Product_Home = [{Oder_Image:1}];
+        this.Data_List_Product_Home = [{Oder_Image:100}];
       }
   }
   }
