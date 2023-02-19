1bd13cc6582d71afce109935510ff34e5b54b906
ThaiPt <vuthaind@gmail.com>
2023-02-19 13:23:46 +0700

    update9

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -11,7 +11,7 @@ const Route = useRoute();
 export const useCounterStore = defineStore('counter', {
   state: () => {
     return {
-      domain_Backend:'http://127.0.0.1:8000',domain_Frontend:'http://127.0.0.1:5173',Language:'vi',Dark:true,Table_Language:false,Table_Language_sm:false,Open_Dark:'darkk',Table_Nav:1,Table_Sidebar:1,Path_Route:useRoute(),
+      domain_Backend:'http://127.0.0.1:8000',domain_Frontend:'https://www.webtoday.store/',Language:'vi',Dark:true,Table_Language:false,Table_Language_sm:false,Open_Dark:'darkk',Table_Nav:1,Table_Sidebar:1,Path_Route:useRoute(),
       Link_Path_Route:{"Home":"/","Web_Template":{Sell_Accounts:"/Product/SellAccounts",Construction_Industry:"/Product/ConstructionIndustry",Cosmetics_Industry:"/Product/CosmeticsIndustry"},Introduce:"/Introduce",Contact:"/Contact"},
       Link_Image:["https://d1hjkbq40fs2x4.cloudfront.net/2017-08-21/files/landscape-photography_1645.jpg","https://img.meta.com.vn/Data/image/2022/01/13/anh-dep-thien-nhien-3.jpg","https://d1hjkbq40fs2x4.cloudfront.net/2020-01-09/files/canon-landscape-photography-preparation-tips_1968-1s.jpg"],
       Oder_Image:0,count:0,Table_Informations:false,Image_Language:'/assets/vn-2ac15179.svg',Text_Array:['Facebook','Zalo','Gọi điện'],class_1:'bg-gray-100 w-[15px] h-[5px] rounded opacity-50',
