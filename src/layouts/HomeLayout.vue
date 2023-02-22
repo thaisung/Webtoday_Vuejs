056b9fc9538e9e9934530dd411bf997062fd88b3
ThaiPt <vuthaind@gmail.com>
2023-02-22 12:13:35 +0700

    thh

--- src/layouts/HomeLayout.vue
+++ src/layouts/HomeLayout.vue
@@ -1,15 +1,14 @@
 <template>
     <div class="flex flex-col min-h-screen min-w-screen bg-white dark:bg-neutral-900 relative" >
-        <div class="px-6 2xl:px-[70px] border-b border-neutral-200 dark:border-neutral-700 fixed top-0 left-0 right-0 z-40 ">
-            <hea-der />
-        </div>
-        <div class="flex flex-col  px-6 2xl:px-[70px]  items-center grow " >
-            <router-view />
-        </div>
-        <div class="px-6 2xl:px-[70px] border-t border-neutral-200 dark:border-neutral-700 ">
-            <foo-ter />
-        </div>
-        <chatbook-facebook />
+      <div class="px-6 2xl:px-[70px] border-b border-neutral-200 dark:border-neutral-700 fixed top-0 left-0 right-0 z-40 ">
+          <hea-der />
+      </div>
+      <div class="flex flex-col  px-6 2xl:px-[70px]  items-center grow " >
+          <router-view />
+      </div>
+      <div class="px-6 2xl:px-[70px] border-t border-neutral-200 dark:border-neutral-700 ">
+          <foo-ter />
+      </div>
     </div>
 </template>
   
@@ -18,7 +17,6 @@
   // Import thành phần (components) NavBar, SideBar, FooterBar để sử dụng
   import HeaDer from '@/components/HeaDer.vue'
   import FooTer from '@/components/FooTer.vue'
-  import ChatbootFacebook from '@/components/ChatbootFacebook.vue'
 
   import { useCounterStore } from '@/stores/counter';
 
@@ -36,7 +34,6 @@
     components: {
       HeaDer,
       FooTer,
-      ChatbootFacebook,
     }
   }
   </script>
